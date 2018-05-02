5090274b90bc4dba2239385d380a5d9c616e9d62
pgodey <38923355+pgodey@users.noreply.github.com>
2018-05-02 15:08:59 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,28 @@
-#!/bin/sh
+#!/bin/sh -x
 
-sed -i "s/\(ldapUrl=\).*\$/\1${LDAP_HOST}/" /opt/adap/conf/general.properties
-sed -i "s/\(ldapPort=\).*\$/\1${LDAP_PORT}/" /opt/adap/conf/general.properties
-sed -i "s/\(adapPort=\).*\$/\1${ADAP_PORT}/" /opt/adap/conf/general.properties
-sed -i "s/\(tokenTimeOutSec=\).*\$/\1${TOKEN_TIMEOUT}/" /opt/adap/conf/general.properties
+#sed -i "s/\(ldapUrl=\).*\$/\1${LDAP_HOST}/" /opt/adap/conf/general.properties
+#sed -i "s/\(ldapPort=\).*\$/\1${LDAP_PORT}/" /opt/adap/conf/general.properties
+#sed -i "s/\(adapPort=\).*\$/\1${ADAP_PORT}/" /opt/adap/conf/general.properties
+#sed -i "s/\(tokenTimeOutSec=\).*\$/\1${TOKEN_TIMEOUT}/" /opt/adap/conf/general.properties
 
-/opt/adap/bin/adap.sh
+GENERAL=/opt/adap/conf/general.properties
+
+echo "ldapUrl=${LDAP_HOST}" > ${GENERAL}
+echo "ldapPort=${LDAP_PORT}" >> ${GENERAL}
+echo "adapPort=${ADAP_PORT}" >> ${GENERAL}
+echo "tokenTimeOutSec=${TOKEN_TIMEOUT}" >> ${GENERAL}
+
+SSL=/opt/adap/conf/ssl.properties
+
+echo "useSSL=${SSL_USE}" > ${SSL}
+echo "sslPort=${SSL_PORT}" >> ${SSL}
+echo "trustStore=${SSL_TRUST_STORE}" >> ${SSL}
+echo "trustStorePwd=${SSL_TRUST_STORE_PSWD}" >> ${SSL}
+
+POOLING=/opt/adap/conf/pooling.properties
+
+echo "alwaysAuthenticate=${POOL_ALWAYS_AUTH}" > ${POOLING}
+echo "maxSize=${POOL_MAX_SIZE}" > ${POOLING}
+echo "timeOut=${POOL_TIMEOUT}" > ${POOLING}
+
+exec /opt/adap/bin/adap.sh
