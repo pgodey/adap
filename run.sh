52acd54ed642be8d7bd10dae9ea557e1ee066247
pgodey <38923355+pgodey@users.noreply.github.com>
2018-05-02 16:55:08 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/sh
 #
 #sed -i "s/\(ldapUrl=\).*\$/\1${LDAP_HOST}/" /opt/adap/conf/general.properties
 #sed -i "s/\(ldapPort=\).*\$/\1${LDAP_PORT}/" /opt/adap/conf/general.properties
