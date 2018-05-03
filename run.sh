3a2b6206d7a077632e3676126e30ce5e01513239
pgodey <38923355+pgodey@users.noreply.github.com>
2018-05-02 17:33:10 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,55 +1,48 @@
 #!/bin/sh
-#
-#sed -i "s/\(ldapUrl=\).*\$/\1${LDAP_HOST}/" /opt/adap/conf/general.properties
-#sed -i "s/\(ldapPort=\).*\$/\1${LDAP_PORT}/" /opt/adap/conf/general.properties
-#sed -i "s/\(adapPort=\).*\$/\1${ADAP_PORT}/" /opt/adap/conf/general.properties
-#sed -i "s/\(tokenTimeOutSec=\).*\$/\1${TOKEN_TIMEOUT}/" /opt/adap/conf/general.properties
-#
+
 GENERAL=/opt/adap/conf/general.properties
-echo "ldapUrl=${LDAP_HOST}" > ${GENERAL}
+
+echo "ldapUrl=${LDAP_HOST}" > ${GENERAL} 
 echo "ldapPort=${LDAP_PORT}" >> ${GENERAL}
 echo "adapPort=${ADAP_PORT}" >> ${GENERAL}
 echo "tokenTimeOutSec=${TOKEN_TIMEOUT}" >> ${GENERAL}
-#
-echo "GENERAL=${GENERAL}"
-echo "ldapUrl=${LDAP_HOST}"
-echo "ldapPort=${LDAP_PORT}"
-echo "adapPort=${ADAP_PORT}"
-echo "tokenTimeOutSec=${TOKEN_TIMEOUT}"
-#
+
 SSL=/opt/adap/conf/ssl.properties
-#
+
 echo "useSSL=${SSL_USE}" > ${SSL}
 echo "sslPort=${SSL_PORT}" >> ${SSL}
 echo "trustStore=${SSL_TRUST_STORE}" >> ${SSL}
 echo "trustStorePwd=${SSL_TRUST_STORE_PSWD}" >> ${SSL}
-#
-echo "SSL=${SSL}"
-echo "useSSL=${SSL_USE}"
-echo "sslPort=${SSL_PORT}"
-echo "trustStore=${SSL_TRUST_STORE}"
-echo "trustStorePwd=${SSL_TRUST_STORE_PSWD}"
-#
+
 POOLING=/opt/adap/conf/pooling.properties
-#
+
 echo "alwaysAuthenticate=${POOL_ALWAYS_AUTH}" > ${POOLING}
 echo "maxSize=${POOL_MAXSIZE}" >> ${POOLING}
 echo "timeOut=${POOL_TIMEOUT}" >> ${POOLING}
-#
-echo "POOLING=${POOLING}"
-echo "alwaysAuthenticate=${POOL_ALWAYS_AUTH}"
-echo "maxSize=${POOL_MAXSIZE}"
-echo "timeOut=${POOL_TIMEOUT}"
-#
-echo "find . -print"
-find . -print
-echo "du -a ."
-du -a .
-echo "ls -ltr /opt/adap/bin"
-ls -ltr /opt/adap/bin
-echo "cat /opt/adap/bin/adap.sh"
-cat /opt/adap/bin/adap.sh
-#
-echo "/bin/sh /opt/adap/bin/adap.sh"
-/bin/sh /opt/adap/bin/adap.sh
-#
+
+# overrides Java executable
+JAVACMD=
+
+PRGDIR=`dirname "$0"`
+BASEDIR=`cd "$PRGDIR/.." >/dev/null; pwd`
+
+# If a specific java binary isn't specified search for the standard 'java' binary
+if [ -z "$JAVACMD" ] ; then
+  if [ -n "$JAVA_HOME"  ] ; then
+    JAVACMD="$JAVA_HOME/bin/java"
+  else
+    JAVACMD=`which java`
+  fi
+fi
+
+if [ ! -x "$JAVACMD" ] ; then
+  echo "Error: JAVA_HOME is not defined correctly." 1>&2
+  echo "  We cannot execute $JAVACMD" 1>&2
+  exit 1
+fi
+
+REPO="$BASEDIR"/lib
+
+CLASSPATH="$REPO"/*
+
+"$JAVACMD" -DADAP_HOME="$BASEDIR" -cp "$CLASSPATH" com.rli.web.http.service.adap.alone.JettyServer "$@"
